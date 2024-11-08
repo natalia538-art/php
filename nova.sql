34f89ecf6db78de6200d99933b569cb408f3b97d
natalia <nataliast538@gmail.com>
2024-11-08 08:35:53 -0300

    v7

--- nova.sql
+++ nova.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 25-Out-2024 às 13:06
+-- Tempo de geração: 07-Nov-2024 às 15:56
 -- Versão do servidor: 10.4.27-MariaDB
 -- versão do PHP: 8.2.0
 
@@ -29,14 +29,22 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `person` (
   `idperson` int(11) NOT NULL,
-  `nome` varchar(45) DEFAULT NULL,
-  `sobrenome` varchar(45) DEFAULT NULL,
+  `nome` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `sobrenome` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `cpf` decimal(11,0) DEFAULT NULL,
-  `email` varchar(45) DEFAULT NULL,
-  `telefone` varchar(45) DEFAULT NULL,
-  `login` varchar(45) DEFAULT NULL,
-  `senha` varchar(45) DEFAULT NULL
+  `email` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `telefone` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `login` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `senha` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `admin` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `person`
+--
+
+INSERT INTO `person` (`idperson`, `nome`, `sobrenome`, `cpf`, `email`, `telefone`, `login`, `senha`, `admin`) VALUES
+(1, 'Thierry', 'Augusto', '12345678925', '', '', 'thierry', '983e91f7b28c8ac201efe87a88ccc519', 0);
 
 --
 -- Índices para tabelas despejadas
@@ -58,7 +66,7 @@ ALTER TABLE `person`
 -- AUTO_INCREMENT de tabela `person`
 --
 ALTER TABLE `person`
-  MODIFY `idperson` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idperson` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
